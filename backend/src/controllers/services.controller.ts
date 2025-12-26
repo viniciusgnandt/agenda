ea1c5a9486a4cabb06ae7e34e1099682b82c5aae
Vinicius Gnandt <viniciusgnandt@hotmail.com>
2025-12-26 19:46:48 -0300

    commit

--- backend/src/controllers/services.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-import { services } from "../data/services.mock";
-
-export function listServices(req: Request, res: Response) {
-  return res.json(services);
-}
--- backend/src/data/services.mock.ts
+++
@@ -1,5 +0,0 @@
-export const services = [
-  { id: 1, name: "Corte Masculino", price: 40, duration: 30 },
-  { id: 2, name: "Barba", price: 30, duration: 20 },
-  { id: 3, name: "Corte + Barba", price: 65, duration: 50 }
-];
