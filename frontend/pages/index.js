e6a40c46a7b8f032b84aa60ecd9f04b365ed53dc
Vinicius Gnandt <viniciusgnandt@hotmail.com>
2025-12-26 17:07:49 -0300

    commiting project

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div style={{fontFamily: 'Arial, sans-serif', padding: 40}}>
+      <h1>Agenda - Frontend</h1>
+      <p>Frontend container is running.</p>
+    </div>
+  )
+}
