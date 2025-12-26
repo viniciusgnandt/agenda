8a0914488a25cd4fad1950dcd463bd9d0be3fdfb
Vinicius Gnandt <viniciusgnandt@hotmail.com>
2025-12-26 19:13:54 -0300

    commitback agian

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,6 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
